eab4591c57082c37c661cbaa4649df0882af03e2
Matthew Elder <eltair@gmail.com>
2011-07-03 15:12:37 -0700

    Add new interfaces for handling timer events

---
+++ src/net/swapspace/apps/wd/DisplayHandler.java
@@ -0,0 +1,5 @@
+package net.swapspace.apps.wd;
+
+public interface DisplayHandler {
+    public void handleChange(long timeLeft);
+}
---
+++ src/net/swapspace/apps/wd/NotificationHandler.java
@@ -0,0 +1,5 @@
+package net.swapspace.apps.wd;
+
+public interface NotificationHandler {
+    public void handleComplete();
+}
